016eeeccef50b84f445370cc7ea177130212f2bc
Leonardo Saraiva <75868207+lsaraivafilho@users.noreply.github.com>
2022-02-08 19:10:02 -0600

    Add files via upload

---
+++ PHPFunctions.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>PHPFunctions</title>
+<?php // 1-
+$original_date = "2019-03-31";
+ 
+// Creating timestamp from given date
+$timestamp = strtotime($original_date);
+ 
+// Creating new date format from that timestamp
+$new_date = date("m-d-Y", $timestamp);
+echo "mm/dd/yyyy Format : $new_date <br>"; // Outputs: 03-31-2019
+?>
+<?php // 2
+// Creating timestamp from given date
+$timestamp = strtotime($original_date);
+ 
+// Creating new date format from that timestamp
+$new_date = date("d-m-Y", $timestamp);
+echo "dd/mm/yyyy Format : $new_date <br>" // Outputs: 31-03-2019
+?>
+<?php // 3-
+
+$my_str1 = '    Leonardo Saraiva Filho      ';
+echo strlen($my_str1); // Outputs: 32
+ echo "<br>";
+ $trimmed = rtrim($my_str1);
+ var_dump($trimmed);
+echo "<br>";
+
+$my_str2 = '             Leo Saraiva Filho                ';
+echo strlen($my_str2); // Outputs: 46
+echo "<br>";
+$trimmed = ltrim($my_str2);
+var_dump($trimmed);
+echo "<br>";
+echo strtolower($my_str2);
+echo "<br>";
+?>
+
+<?php
+$input = "1234567890"; 
+$output = substr($input, -10, -7) . "-" . substr($input, -7, -4) . "-" . substr($input, -4); 
+echo $output;
+echo "<br>";
+?>
+<?php
+$number = 123456;
+$format_number = number_format($number);
+echo "$ $format_number";
+
+?>
+
+
+</head>
+<body>
+
+
+</body>
+</html>
